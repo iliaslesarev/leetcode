92b5156f6761870dbd5359446e968f1d94b194f6
ilia.slesarev <slesarev.pr@gmail.com>
2022-12-21 21:25:11 +0100

    206 Reverse Linked List

---
+++ 0_1000/201_300/206_reverse_linked_list.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        res = None
+
+        while head:
+            tmp = head.next
+            head.next = res
+            res = head
+            head = tmp
+
+        return res
+
+list1 = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+list2 = Solution().reverseList(list1)
+a = 5
