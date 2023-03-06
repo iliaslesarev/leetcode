a5062e8bdb6c51c51b9a9dce0d6e6789a941d510
Ilia Slesarev <slesarev.pr@gmail.com>
2023-03-07 00:10:22 +0100

    438. Find All Anagrams in a String

---
+++ 0_1000/401_500/438_find_all_anagrams_in_a_string.py
@@ -0,0 +1,33 @@
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        ns, np = len(s), len(p)
+        if ns < np:
+            return []
+
+        counter_p = Counter(p)
+        counter_s = Counter()
+
+        result = []
+
+        for i in range(ns):
+            counter_s[s[i]] += 1
+
+            if i >= np:
+                if counter_s[s[i - np]] == 1:
+                    del counter_s[s[i - np]]
+                else:
+                    counter_s[s[i - np]] -= 1
+
+            if counter_s == counter_p:
+                result.append(i - np + 1)
+        return result
+
+
+# print(Solution().findAnagrams(s="cbaebabacd", p="abc"))
+print(Solution().findAnagrams(
+    s="eklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsnifvhmoueklpyqrbgjdwtcaxzsni",
+    p="yqrbgjdwtcaxzsnifvhmou"))
